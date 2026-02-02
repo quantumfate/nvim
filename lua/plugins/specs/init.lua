a6af5859ed357105d20e7e6a594ff3c6f771451f
Leon Connor Holm <62796852+quantumfate@users.noreply.github.com>
2026-02-02 18:58:08 +0100

    Added bufferline

--- lua/plugins/specs/init.lua
+++ lua/plugins/specs/init.lua
@@ -3,6 +3,7 @@ local module_string = "plugins.specs."
 local catppuccin = require(module_string .. "catppuccin")
 local bufferline = require(module_string .. "bufferline")
 
+print(vim.inspect(bufferline))
 return {
 	catppuccin,
 	bufferline
